d044eb40c0fb89227e83e4f636cb3afe3997a8f1
GooseUnloose <gooseunloose@gmail.com>
2025-03-26 23:18:48 +0000

    Add files via upload

---
+++ Hypothesis testing Models.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+
+
+datasets={'caffe','incubator-mxnet','keras','pytorch','tensorflow'}
+
+for i in datasets:
+
+    df1 = pd.read_csv(f'C:/Users/Goose/Desktop/Results/1Phase_{i}_GaussianNB_RAWDATA.csv').fillna("")
+    df2 = pd.read_csv(f'C:/Users/Goose/Desktop/Results/1Phase_{i}_MultinomialNB_RAWDATA.csv').fillna("")
+
+    cols = df1.columns
+    reject = True
+    print(f"\n###{i}###")
+    for i in cols[1:-1]:
+        test1 = df1[i].tolist()
+        test2 = df2[i].tolist()
+
+        t_stat, p_value = stats.ttest_rel(test1,test2)
+    
+        alpha = 0.05
+    
+        #print(f"\ncompareing {i}")
+        if p_value < alpha:
+            #print(f"{i} Reject null, Significant difference between performance")
+            None
+        else:
+            print(f"{i} accept, no significant difference in model performance")
+            reject = False
+        #print(t_stat)
+        #print(p_value)
+    if reject == False:
+        print("we cannot fully reject null model as being better as it outperforms in at least one metric")
+    
+    else:
+        print("We can fully reject null model, as it is surpassed in all criteria")
+
+
